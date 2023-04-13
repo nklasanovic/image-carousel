0ea802da15cc8deced25e544884801007d2b20d5
nebojsaklasanovic <nebojsa.shone9998@gmail.com>
2023-04-13 13:26:20 +0200

    Project commited

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
+import { RxDotFilled } from "react-icons/rx";
+import slides from "./data/Slides";
+
+const App = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const getToSlide = (slideIndex) => {
+    setCurrentIndex(slideIndex);
+  };
+
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  return (
+    <div className="max-w-[1080px] h-[580px] w-full m-auto px-4 py-16 relative group">
+      <div
+        style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+        className="w-full h-full rounded-2xl bg-center bg-cover duration-500"
+      ></div>
+      <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+        <BsChevronCompactLeft onClick={prevSlide} size={30} />
+      </div>
+      <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+        <BsChevronCompactRight onClick={nextSlide} size={30} />
+      </div>
+      <div className="flex top-4 justify-center py-2">
+        {slides.map((slide, slideIndex) => (
+          <div
+            key={slideIndex}
+            onClick={() => getToSlide(slideIndex)}
+            className="text-gray-300 text-3xl cursor-pointer hover:text-red-500 hover:scale-110"
+          >
+            <RxDotFilled />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/data/Slides.jsx
@@ -0,0 +1,31 @@
+const slides = [
+  {
+    url: "https://wallpapercave.com/wp/ixmt8JQ.jpg",
+  },
+
+  {
+    url: "https://cdn.wallpapersafari.com/28/73/TcRMwS.jpg",
+  },
+
+  {
+    url: "https://wallpapers.com/images/featured/2ygv7ssy2k0lxlzu.jpg",
+  },
+
+  {
+    url: "https://wallpaperset.com/w/full/3/0/f/120853.jpg",
+  },
+
+  {
+    url: "https://wallpapercave.com/wp/3a0T946.jpg",
+  },
+
+  {
+    url: "https://wallpapercave.com/wp/wp2656294.jpg",
+  },
+
+  {
+    url: "https://www.pixelstalk.net/wp-content/uploads/images6/Nature-HD-Wallpaper-Free-download.jpg",
+  },
+];
+
+export default slides;
